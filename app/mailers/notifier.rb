f409a6718c695607556c3032bac2c88957de7bb5
Andrew <dreamsoftech88@gmail.com>
2018-04-24 17:12:54 +0800

    followup for email notifications

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,8 +2,7 @@ class Notifier < ActionMailer::Base
   default from: "support@affluence.org"
 
   def event_registration(notification_tracker)
-    #mail( :to => notification_tracker.user.email,
-          mail( :to => 'umamahesh.seeram@seneca-global.com',
+    mail( :to => notification_tracker.user.email,
           :subject => notification_tracker.subject )
   end
 end
