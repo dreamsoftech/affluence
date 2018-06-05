c44b5962fe58722a76f1c5e87b3adbedf518a6cd
Andrew <dreamsoftech88@gmail.com>
2018-06-05 16:36:26 +0800

    added offers module in admin

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -12,4 +12,10 @@ class OffersController < ApplicationController
     @shopping_offers = Offer.where("category like 'Shopping'", :order => 'created_at asc')
     @financial_offers = Offer.where("category like 'Financial'", :order => 'created_at asc')
   end
+
+  def activate
+  offer = Offer.find(params[:id])
+  offer.promotion.activate_promotion_for_member(current_user)
+  redirect_to offer.link
+  end
 end
