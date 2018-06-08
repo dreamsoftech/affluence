a663ed2522c278b8dc9bb56934a9f0356f5732a8
Andrew <dreamsoftech88@gmail.com>
2018-06-08 22:45:37 +0800

    added more activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,10 @@
 class ActivitiesController < ApplicationController
 
+  def index
+    @latest_activities = Kaminari.paginate_array(Activity.all_by_privacy_setting).page(params[:page]).per(3)
+  end
 
-  def latest
+ def latest
     if params["my_connections"] == 'true'
       @latest_activities = current_user.connections_activities
     else
