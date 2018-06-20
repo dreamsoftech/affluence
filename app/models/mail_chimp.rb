c4ce558e0fcb8ed535ed83144f285e6d11dda34b
Andrew <dreamsoftech88@gmail.com>
2018-06-20 14:21:57 +0800

    added rescue to mail chimp

--- app/models/mail_chimp.rb
+++ app/models/mail_chimp.rb
@@ -3,13 +3,16 @@ class MailChimp
 
   def self.add_user(user)
     h = Hominid::API.new(MAIL_CHIMP[Rails.env]['api_key'])
-    h.list_subscribe(MAIL_CHIMP[Rails.env]['subscriber_list'], user.email, {'FNAME' => user.profile.first_name, 'LNAME' => user.profile.last_name}, 'html', false, true, true, false)
+    begin
+      h.list_subscribe(MAIL_CHIMP[Rails.env]['subscriber_list'], user.email, {'FNAME' => user.profile.first_name, 'LNAME' => user.profile.last_name}, 'html', false, true, true, false)
+    rescue
+    end
   end
 
   def self.unsubscribe_user(user)
     h = Hominid::API.new(MAIL_CHIMP[Rails.env]['api_key'])
     begin
-    h.list_unsubscribe(MAIL_CHIMP[Rails.env]['subscriber_list'], user.email)
+      h.list_unsubscribe(MAIL_CHIMP[Rails.env]['subscriber_list'], user.email)
     rescue
       h.list_subscribe(MAIL_CHIMP[Rails.env]['subscriber_list'], user.email, {'FNAME' => user.profile.first_name, 'LNAME' => user.profile.last_name}, 'html', false, true, true, false)
     end
