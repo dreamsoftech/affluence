b025c071004825c7d2951fafc7f7b43dd17991ef
Andrew <dreamsoftech88@gmail.com>
2018-08-16 17:56:54 +0800

    updated photo validation for upload from ie8

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,7 +5,7 @@ class Photo < ActiveRecord::Base
 
   validates_attachment_presence :image, :if => lambda { |images| !images.image_file_name.nil?}
   validates_attachment_size :image, :less_than => 5.megabyte
-  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+  validates_attachment_content_type :image, :content_type => ['image/pjpeg', 'image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
   unless Rails.env.development?
     paperclip_opts = {:storage => :s3,
