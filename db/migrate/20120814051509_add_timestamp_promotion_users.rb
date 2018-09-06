5be00c20fd268bcd17e9574a3a869ed78750bee5
Andrew <dreamsoftech88@gmail.com>
2018-09-06 19:34:39 +0800

    added photo to winshares

--- db/migrate/20120814051509_add_timestamp_promotion_users.rb
+++ db/migrate/20120814051509_add_timestamp_promotion_users.rb
@@ -1,7 +1,10 @@
 class AddTimestampPromotionUsers < ActiveRecord::Migration
   def up
-    add_column(:promotions_users, :created_at, :datetime)
+    add_column :promotions_users, :created_at, :datetime
   end
 
+  #def down
+   # remove_column :promotions_users, :created_at, :datetime
+  #end
 
 end
