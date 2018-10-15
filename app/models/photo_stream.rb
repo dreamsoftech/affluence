3badf53e8a567e596ec34bd17e622b49f9209b26
Andrew <dreamsoftech88@gmail.com>
2018-10-15 21:20:37 +0800

    changed albumn to album, removed default title to photo album

--- app/models/photo_stream.rb
+++ app/models/photo_stream.rb
@@ -2,11 +2,10 @@ class PhotoStream < ActiveRecord::Base
   belongs_to :profile
   has_many :photos, :as => :photoable, :dependent => :destroy
 
-
-
+  
   before_save :check_title
 
   def check_title
-    self.title = 'Untitled Albumn' if self.title.strip! == ''
+    self.title = 'Untitled Album' if (self.title.nil? || self.title.strip! == '') 
   end
 end
