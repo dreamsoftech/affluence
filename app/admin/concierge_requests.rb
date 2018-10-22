0dcddeb14aaaa38026f111afe8ca7790f899d6b8
Andrew <dreamsoftech88@gmail.com>
2018-10-22 18:06:06 +0800

    added message subject to conversations

--- app/admin/concierge_requests.rb
+++ app/admin/concierge_requests.rb
@@ -129,7 +129,8 @@ ActiveAdmin.register ConciergeRequest, :namespace=> :admin do
     if conversation.nil?
       conversation = Conversation.new(:messages_attributes => { "0" => { "body" => params[:message][:body], :subject => 'title'}})
     else
-      conversation.messages << Message.new(:subject => "Concierge Request", :body => params[:message][:body])
+      last_interaction_message = concierge_request.interactions.last.interactable
+      conversation.messages << Message.new(:subject => "Concierge Request", :body => params[:message][:body], :subject => last_interaction_message.subject)
     end
     conversation.messages.last.sender = User.find(concierge_request.operator_id)
     conversation.messages.last.recipient = User.find(concierge_request.user_id)
