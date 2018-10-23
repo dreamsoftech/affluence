ba9d88ce6ebe56dbcf6ccb954d72118deb0ae992
Andrew <dreamsoftech88@gmail.com>
2018-10-23 16:15:22 +0800

    connection request can be made only between two users of role as member

--- app/models/message.rb
+++ app/models/message.rb
@@ -26,31 +26,36 @@ class Message < ActiveRecord::Base
   end
 
   def self.search_subject_body (query)
-        find_by_sql(
-        "      with distinct_subjects as (
+    find_by_sql(
+      "      with distinct_subjects as (
             select max(id) as subject_rank,subject as distinct_subject from messages where (to_tsvector('english', COALESCE(subject,'') || ' ' || COALESCE(body,''))  @@ plainto_tsquery('english', '#{query}')) group by subject order by subject_rank desc
         )
         select distinct_subjects.subject_rank, messages.*, rank() over (partition by subject order by created_at desc) as internal_rank, body
         from messages left outer join distinct_subjects on messages.subject=distinct_subjects.distinct_subject
         where to_tsvector('english', COALESCE(subject,'') || ' ' || COALESCE(body,''))  @@ plainto_tsquery('english', '#{query}') order by subject_rank desc, internal_rank asc
-       ")
+      ")
   end
   def make_connection_req
-    _conn = Connection.where(:user_id => self.sender_id, :friend_id => self.recipient_id)
-    if _conn.blank?
-      _conn_req = ConnectionRequest.where(:requestor_id => self.sender_id, :requestee_id => self.recipient_id)
-      if _conn_req.blank?
-        check_record = check_reverse_exists_or_not(self.sender_id, self.recipient_id)
-        if !check_record.blank?
-          check_and_clear_connection_request(self.recipient_id, self.sender_id, check_record)
+    sender = User.find self.sender_id
+    recipient = User.find self.recipient_id
+      
+    if sender.member? && recipient.member?
+      _conn = Connection.where(:user_id => self.sender_id, :friend_id => self.recipient_id)
+      if _conn.blank?
+        _conn_req = ConnectionRequest.where(:requestor_id => self.sender_id, :requestee_id => self.recipient_id)
+        if _conn_req.blank?
+          check_record = check_reverse_exists_or_not(self.sender_id, self.recipient_id)
+          if !check_record.blank?
+            check_and_clear_connection_request(self.recipient_id, self.sender_id, check_record)
+          else
+            # create connection request
+            conec_req = ConnectionRequest.create(:requestor_id => self.sender_id, :requestee_id => self.recipient_id)
+          end
         else
-          # create connection request
-          conec_req = ConnectionRequest.create(:requestor_id => self.sender_id, :requestee_id => self.recipient_id)
         end
       else
+        # connection exists. dont create any connection
       end
-    else
-      # connection exists. dont create any connection
     end
   end
 
@@ -92,7 +97,7 @@ class Message < ActiveRecord::Base
 
   def notify_message_through_email
     NotificationTracker.create(:user_id => self.sender_id, :channel => 'email', :subject => "message",
-                               :status => 'pending', :notifiable_id => self.id, :notifiable_type => 'Message',
-                               :notifiable_mode => 1, :scheduled_date => Date.today)
+      :status => 'pending', :notifiable_id => self.id, :notifiable_type => 'Message',
+      :notifiable_mode => 1, :scheduled_date => Date.today)
   end
 end
